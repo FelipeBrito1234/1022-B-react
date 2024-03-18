6afded1493381a6485805c6b7bec482bc096ebbd
FelipeBrito1234 <felipe.ferrreira@estudante.ifms.edu.br>
2024-03-18 11:44:48 -0400

    18/03/2024

--- src/componentes/MeuComponente.tsx
+++ src/componentes/MeuComponente.tsx
@@ -1,16 +1,21 @@
-
 import './MeuComponente.css'
-//Criar nosso primeiro componente.
-//Esse componente será o componente do nosso HEADER.
+//Criar nosso primeiro componente. 
+//Esse componente será o componenete do nosso HEADER. 
 //No header temos 300px de altura ele é do tamanho da tela
-//Ele tem uma borda amarela de 2px solida.
-//Dentro da tag header nos temos uma div com texto "logo" de
-//      tamanho 200 x 200px centralizada na vertical a 30px da lateral esquerda
-
+// Ele tem uma borda amarela de 2px solida.
+// Dentro da tag header nos temos uma div com o texto "logo" de
+//    tamanho 200 x 200px centralizada na vertical a 30px da lateral esquerda
 function MeuComponente(){
-    return {
-       <header className='header'>
-       
-    }
+    let variavel="Logo";
+    return (
+        <header className='header'>
+            <div className='logo'>
+                {variavel}
+            </div>
+        </header>
+    )
 }
+
+
+
 export default MeuComponente;
