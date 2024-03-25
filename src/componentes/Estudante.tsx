cdde48abdcd6e246ec210d9bca17350f58523624
FelipeBrito1234 <118356252+FelipeBrito1234@users.noreply.github.com>
2024-03-25 00:23:34 -0300

    Create Estudante.tsx

---
+++ src/componentes/Estudante.tsx
@@ -0,0 +1,14 @@
+import './Estudante.css'
+
+function Estudante() {
+    return (
+        <div className='box-estudante'>
+                <img src="https://github.com/joaovscaetano.png" alt="Foto Estudante" />
+                <h3 className='nome-estudante'>João Victor da Silva Caetano</h3>
+                <p className='descricao-estudante'>Estudante do IFMS nas matérias de Desenvolvimento - Desenvolvimento Web</p>
+                <a href="https://github.com/joaovscaetano">GitHub</a>
+            </div>
+    )
+}
+
+export default Estudante
