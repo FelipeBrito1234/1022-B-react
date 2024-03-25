1b4de8f6bbbc7c07b902a7760606d9e3379cf3e4
FelipeBrito1234 <118356252+FelipeBrito1234@users.noreply.github.com>
2024-03-25 00:17:15 -0300

    Update Main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,6 +1,12 @@
-function Main(){
+import './Main.css'
+import Estudante from './Estudante/Estudante'
+
+function Main() {
     return(
-        <main>Main</main>
+        <main>
+            <Estudante />
+        </main>
     )
 }
+
 export default Main
